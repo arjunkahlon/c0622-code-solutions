8cbf44246f5f1fcb7753de36c1581218b72afae4
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-11 17:34:16 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+var header = document.querySelector('h1');
+console.log(header);
+console.dir(header);
+
+var explain = document.querySelector('#explanation');
+console.log(explain);
+console.dir(explain);
+
+var hint = document.querySelector('.hint');
+console.log(hint);
+console.dir(hint);
+
+var pList = document.querySelectorAll('p');
+console.log(pList);
+
+var exampleList = document.querySelectorAll('.example-link');
+console.log(exampleList);
