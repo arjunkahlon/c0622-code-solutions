d455d414156a2ddef7fd182dfc508704e6070d24
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-13 00:54:57 +0000

    First stages of typing tutor exercise. Program successfull creates nodeList from span elements in HTML.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,16 @@
+function handleType(event) {
+  // console.log(event.keyCode);
+
+}
+
+// function logSpanCollection(object) {
+//   for (let i = 0; i < object.length; i++) {
+//     console.log(object[i].innerText);
+//   }
+// }
+
+// var $panCollection = document.querySelectorAll('span');
+// console.log($panCollection);
+
+// logSpanCollection($panCollection);
+document.addEventListener('keydown', handleType);
