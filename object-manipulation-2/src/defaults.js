663d66bc5692a3234e32a49b53b8db77a0310426
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-18 17:08:14 +0000

    Reimplemented the defaults algorithm without concerting parameter objects to arrays. Function now utilizes a for in loop rather.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,11 +1,8 @@
 /* exported defaults */
 function defaults(target, source) {
-  var sourceArr = Object.getOwnPropertyNames(source);
-  var targetArr = Object.getOwnPropertyNames(target);
-
-  for (let i = 0; i < sourceArr.length; i++) {
-    if (!(targetArr.includes(sourceArr[i]))) {
-      target[sourceArr[i]] = source[sourceArr[i]];
+  for (var key in source) {
+    if (!(key in target)) {
+      target[key] = source[key];
     }
   }
 }
