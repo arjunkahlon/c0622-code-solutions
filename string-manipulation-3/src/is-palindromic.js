bb896000e254b93cf4c5564c56028285579b7953
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-26 19:27:36 +0000

    Completed the first 3 algorithms for string-manipulation-3.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,28 @@
+/* exported isPalindromic */
+// First we will need to remove the empty characters as we are basing the
+//  palindrome condition based sole on the alphabetical characters.
+// Next we will reverse through the string of alphabetical characters, and
+//  concatenate each index into a new string representing the reversed String.
+// Finally, we will perform a stricly equal comparison betwen the alphabetical
+//  string and the reversed string. If they are strictly equal, then the
+//  palindrome condition is met.
+
+function isPalindromic(string) {
+  var newStr = '';
+  var revStr = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      newStr += string[i];
+    }
+  }
+
+  for (let i = newStr.length - 1; i >= 0; i--) {
+    revStr += newStr[i];
+  }
+
+  if (newStr === revStr) {
+    return true;
+  }
+
+  return false;
+}
---
+++ string-manipulation-3/src/reverse-words.js
@@ -0,0 +1,32 @@
+/* exported reverseWords */
+// We begin by reversing the parameter string into a new string
+// We then iterate through the reversed string, and concatenate each
+//  indexed character value into a substring until we reach either an
+//  empty space, or beyond the last index of the string.
+// When this condition is met, we concatentate the substring with the
+//  finalstring and store the result of the expression in the final string.
+// We also account for the last substring which will not
+//  have an empty space concatenated in front of it.
+// Finally, we return the final string.
+function reverseWords(string) {
+  var reverseStr = '';
+  var finalStr = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseStr += string[i];
+  }
+
+  for (let i = 0; i < reverseStr.length; i++) {
+    var subStr = '';
+    while (reverseStr[i] !== ' ' && i < reverseStr.length) {
+      subStr += reverseStr[i];
+      i++;
+    }
+    if (i < reverseStr.length - 1) {
+      finalStr = ' ' + subStr + finalStr;
+    } else {
+      finalStr = subStr + finalStr;
+    }
+  }
+  return finalStr;
+}
