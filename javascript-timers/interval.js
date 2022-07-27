5764d2738f6e4ef6820e05a48718875deb776180
Arjun Kahlon <arjunskahlon@gmail.com>
2022-07-27 17:32:26 +0000

    Completed javascript-timers exercise.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,11 +1,10 @@
 var $heading = document.querySelector('.countdown-display');
 var counter = parseInt($heading.textContent);
-
 var intervalID = setInterval(countDown, 1000);
 
 function countDown() {
   counter--;
-  $heading.textContent = counter;
+  $heading.textContent = counter.toString();
   if (counter === 0) {
     $heading.textContent = '~Earth Beeeelooowww Us~';
     clearInterval(intervalID);
