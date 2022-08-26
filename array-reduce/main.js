d28d2d27337756508d3a6344f31892bb3a7b7738
Arjun Kahlon <arjunskahlon@gmail.com>
2022-08-26 22:42:30 +0000

    Completed array-reduce exercise.

---
+++ array-reduce/main.js
@@ -0,0 +1,40 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = numbers.reduce((previousValue, currentValue) => currentValue + previousValue, 0);
+console.log(`Sum of numbers array: ${sum}`);
+
+const product = numbers.reduce((previousValue, currentValue) => currentValue * previousValue);
+console.log(`Product of numbers array: ${product}`);
+
+const amount = (previousValue, currentValue) => {
+  let currentAmount = currentValue.amount;
+  if (currentValue.type === 'deposit') {
+    currentAmount = previousValue + currentAmount;
+  } else {
+    currentAmount = previousValue - currentAmount;
+  }
+  return currentAmount;
+};
+
+const balance = account.reduce(amount, 0);
+console.log(`Balance of account: ${balance}`);
+
+const composite = traits.reduce((previousValue, currentValue) => Object.assign(previousValue, currentValue));
+console.log('Composite: ', composite);
