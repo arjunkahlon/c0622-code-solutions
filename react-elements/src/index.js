bc62cc494e937d55cf4e6e7ee0be3a8eed85269c
Arjun Kahlon <arjunskahlon@gmail.com>
2022-08-30 00:47:14 +0000

    Completed react elements exercise.

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const header = React.createElement(
   'h1',
@@ -7,4 +7,5 @@ const header = React.createElement(
   [...'Hello, React!']
 );
 
-ReactDOM.render(header, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(header);
