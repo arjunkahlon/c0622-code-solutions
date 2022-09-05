a20238805624c4adef362145a844cd231e0c9837
Arjun Kahlon <arjunskahlon@gmail.com>
2022-09-05 01:27:24 +0000

    Completed react accordion component challenge.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,23 +4,51 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentIndex: null
+      currentNumber: null
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(number) {
-    const clickedIndex = parseInt(number) - 1;
-    if (clickedIndex === this.currentIndex) {
-      this.setState({ currentIndex: null });
+
+    if (number === this.state.currentNumber) {
+      this.setState({
+        currentNumber: null
+      });
     } else {
-      this.setState({ currentIndex: clickedIndex });
+      this.setState({
+        currentNumber: number
+      });
     }
   }
 
   render() {
-    return <div></div>;
+    const topicsList = this.props.topics;
+    const carouselElement = topicsList.map(topic => {
+      if (topic.number === this.state.currentNumber) {
+        return (
+          <li key={topic.number} onClick={() => this.handleClick(topic.number)}>
+            <div>
+              <h2 className='topic-header'>{topic.name}</h2>
+              <p className='topic-description'>{topic.description}</p>
+            </div>
+          </li>
+        );
+      } else {
+        return (
+          <li key={topic.number} className='topic-list-item' onClick={() => this.handleClick(topic.number)}>
+            <div>
+              <h2 className='topic-header'>{topic.name}</h2>
+              <p className='topic-description hidden'>{topic.description}</p>
+            </div>
+          </li>
+        );
+      }
+
+    });
+
+    return <ul className='topic-list'>{carouselElement}</ul>;
   }
 
 }
